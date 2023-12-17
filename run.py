72898e1804e9af4efff6ed5c2a13360e0d12a3d1
tildeholmqvist <>
2023-12-17 18:27:43 +0000

    added the class Board and my first defined function

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0, "player": 0}
+
+class Board:
+    """
+    This is the main class. This sets the structure of the game and includes
+    methods for actually playing the game.
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["0" for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+  
