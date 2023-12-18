fe6cc7cc35ae3e78bb9d05a90ab174498591932c
tildeholmqvist <>
2023-12-18 10:43:11 +0000

    changing the builup of the game, starting with instructions and adding couting turns

--- run1.py
+++ run1.py
@@ -1,21 +1,89 @@
-from random import randint
-
-scores = {"computer": 0, "player": 0}
-
+import random
 
 class Board:
     """
     This is the main class. This sets the structure of the game and includes
     methods for actually playing the game.
     """
+    def __init__(self):
+        self.hit = []
+        self.miss = []
+        self.ships_found = 0
+        self.boats = self.generate_random_ships()
 
-    def __init__(self, size, num_ships, name):
-        self.size = size
-        self.board = [["0" for x in range(size)] for y in range(size)]
-        self.num_ships = num_ships
-        self.name = name
-        self.guesses = []
-        self.ships = []
+    def generate_random_ships(self):
+        boats = []
+        while len(boats) <:3
+            new_boat = random.randint(0,48)
+            if new_boat not in boats:
+                boats.append(new_boat)
+        return boats
+
+    def get_shots(self):
+        while True:
+            try:
+                row = int(input("\nPick a row (0-4):")) 
+                if row < 0 or row > 4:
+                    print("Incorrect coordinates. You have to pick a row between 0 and 4.")
+                    continue
+                col = int(input("Pick a column (0-4):"))  
+                if col < 0 or col > 4:
+                    print("Incorrect coordinates. You have to pick a column between 0 and 4.")
+                    continue
+
+                shot = 5 * row + col  
+
+                else:
+                    return shot
+            except ValueError:
+                print("Incorrect entry, Please enter your number.")
+
+    def check_shot(self, shot):
+        if shot in self.boats:
+            self.boats.remove(shot)
+            self.hit.append(shot)
+            self.ships_found += 1
+            print (f"That was a HIT! Total ships found: {self.ships_found}\n")
+            return True
+        else:
+            self.miss.append(shot)
+            return False
+
+        def display_board(self):
+        print(f"\n THE BATTLEFIELD \n")
+        print("    0  1  2  3  4 ")
+        place = 0
+        for x in range(5):
+            row = ""
+            for y in range(5):
+                if place in self.hit:
+                    ch = " X "
+                elif place in self.miss:
+                    ch = " O "
+                else:
+                    ch = " * "
+                row += ch
+                place += 1
+            print(x, "", row)
+
+    def play_game(self):
+        player_name = input("Enter your username: ")
+        print("\n")
+        print(f"WELCOME {player_name}! Are you ready for a game of Battleship?")
+        print("\n")
+
+        print("You have a total of 10 turns to sink 3 hidden ships.")
+        print("Guess a row and a column between 0 and 4.")
+        print("If you HIT a ship you will see X, if you miss a ship you will see 0.")
+        print("If you want to quit the game, type 'exit'.")
+        print("GOOD LUCK!")
+        print("\n")
+
+        turns_remaining = 10
+
+        for i in range(turns_remaining):
+            
+
 
     def welcome(self):
         """
@@ -41,35 +109,6 @@ class Board:
                     self.ships.append((ship_row, ship_col))
                     break
     
-    def player_shot(self, guesses, computer_board):
-        while True:
-            try:
-                row = int(input("\nPick a row (0-4):"))  # Convert input to int
-                if row < 0 or row > 4:
-                    print("Incorrect coordinates. You have to pick a row between 0 and 4.")
-                    continue
-                col = int(input("Pick a column (0-4):"))  # Convert input to int
-                if col < 0 or col > 4:
-                    print("Incorrect coordinates. You have to pick a column between 0 and 4.")
-                    continue
-
-                shot = 5 * row + col  
-
-                if shot in guesses:
-                    print("You have already tried this coordinate. Please try another one.")
-                else:
-                    if shot in computer_board.ships:
-                        print("\nThat was a HIT!\n")
-                        computer_board.ships.remove(shot)
-                        guesses.append(shot)
-                        return True
-                    else:
-                        print("\nThat was a MISS!\n")
-                        guesses.append(shot)
-                        return False
-            except ValueError:
-                print("Please pick a valid number.")
-    
     def computer_shot(self, guesses, player_board):
         while True:
             comp_row = randint(0, self.size -1)
@@ -89,24 +128,6 @@ class Board:
     def check_score(self):
         return len(self.ships) == 0
     
-    def display_board(self, hit, miss, show_ships=False):
-        print(f"\n THE BATTLEFIELD - {self.name.upper()} \n")
-        print("    0  1  2  3  4 ")
-        place = 0
-        for x in range(5):
-            row = ""
-            for y in range(5):
-                if (x, y) in self.ships:
-                    ch = " @ "
-                elif place in hit:
-                    ch = " X "
-                elif place in miss:
-                    ch = "0"
-                else:
-                    ch = " * "
-                row += ch
-                place += 1
-            print(x, "", row)
 
 size = 5
 num_ships = 3
@@ -125,22 +146,16 @@ player_guesses = []
 computer_guesses = []
 
 while True:
-    player_board.display_board(player_guesses, computer_guesses)
     player_shot_result = player_board.player_shot(player_guesses, computer_board)
-    
-    if player_shot_result:
-        scores["player"] += 1
+    player_board.display_board(player_guesses, computer_guesses)
 
     if player_board.check_score():
         print("\nYou WIN! Congratulations, you sank all the computers ships!")
         break
 
-    computer_board.display_board(player_guesses, computer_guesses)
     computer_shot_result = computer_board.computer_shot(computer_guesses, player_board)
+    player_board.display_board(player_guesses, computer_guesses)
 
-    if computer_shot_result:
-        scores["computer"] += 1
-    
-    if player_board.check_score():
+    if computer_board.check_score():
         print("\n You lost... The computer sank all your ships.")
         break
